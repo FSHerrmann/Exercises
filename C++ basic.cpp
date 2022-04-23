58619f73b4c5de2324a334680efc643f8d9b78bd
Felipe Schneider Herrmann <mime.schneider@gmail.com>
2022-04-23 17:24:59 -0300

    Add files via upload

---
+++ C++ basic.cpp
@@ -0,0 +1,30 @@
+/* 1 */
+#include <iostream>
+using namespace std;
+        
+int main()
+{
+cout << "Wellcome :" <<endl;
+cout << "Thats some c++ Exercises";
+return 0;
+}        
+
+/* 2 */
+#include <iostream>
+using namespace std;
+        
+
+{
+cout << "5 + 9 = " <<5+9;
+}      
+
+/* 3 */
+#include <iostream>
+using namespace std;
+
+{
+string name("Felipe");
+cout << "Size of my name (Felipe) is: " <<sizeof(name) << "bytes";  
+
+return 0;
+}
